1ea27dd19b3967f7b4a61ed4d29e1ac0b0ec14f3
Manh Pham <manhpham90vn@icloud.com>
2020-10-26 21:17:03 +0700

    update code

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
 import ComReactNativeDocPreview from 'com.react-native.doc-preview';
 
 const open = () => {
